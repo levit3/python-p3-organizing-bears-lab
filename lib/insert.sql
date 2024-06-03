324820743134fab66585bb0e8998de2fadb126c2
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-06-03 21:20:46 +0300

    completes all operations

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Gentle', true),
+('Rowdy', 5, 'M', 'Black', 'Playful', true),
+('Tabitha', 8, 'F', 'Brown', 'Friendly', true),
+('Sergeant Brown', 12, 'M', 'Grizzly', 'Serious', true),
+('Melissa', 6, 'F', 'Golden', 'Curious', true),
+('Grinch', 15, 'M', 'White', 'Grumpy', true),
+('Wendy', 7, 'F', 'Cinnamon', 'Sweet', true),
+(NULL, 4, 'M', 'Brown', 'Adventurous', true);
